9a83e9a1f4e066b410aa112fcb97c2470757c5ff
zanweichuan1 <110811022+zanweichuan1@users.noreply.github.com>
2022-08-08 18:18:24 +0800

    github-update

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        gitHub项目更新了
     }
 }
