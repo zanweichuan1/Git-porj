5646750bc134b0aad2fdcc62e179b514ff23cb04
zanweichuan <3099065973@qq.com>
2022-08-08 14:51:24 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,5 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!2");
         System.out.println("hello git!3");
-        System.out.println("hello git!4");
     }
 }
